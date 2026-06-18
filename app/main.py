223e951b739e48a3350ee57f8f39494a4f53ef62
xoralife <abidhussainiouu@gmail.com>
2026-06-18 14:54:24 +0500

    multi http

--- app/main.py
+++ app/main.py
@@ -1,11 +1,32 @@
 from fastapi import FastAPI
 from mockData import products
+
+
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Hello World"}
 
-@app.get("/pro")
+
+@app.get("product")
 def get_products():
     return products
+
+
+@app.get("/product/{product_id}")
+def get_one_product(product_id:int):
+    # product = None
+
+    for oneProduct in product:
+        if oneProduct.get("id") == product_id:
+            return oneProduct
+        
+    return{
+        "error"
+    }
+
+    
+    # return {
+    #     "id":product_id
+    # }
